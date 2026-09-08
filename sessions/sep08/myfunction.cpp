0c08f999784ce83348fc0b7cc53b5dda2caece57
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2026-09-08 16:32:23 -0500

    separate compilation demo

---
+++ sessions/sep08/myfunction.cpp
@@ -0,0 +1,10 @@
+#include "myfunction.hpp"
+// the previous line is not strictly needed,
+// but it prevents accidents
+
+int f(int i ) {
+  return i+1;
+}
+
+// compile with "-c" option,
+// and you get a "object file"
