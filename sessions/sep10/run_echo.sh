0e4bc9ea9c11494b60a0ea10118e5f2ef7a12b7f
Victor Eijkhout <eijkhout@tacc.utexas.edu>
2026-09-10 16:46:51 -0500

    sep 10 files

---
+++ sessions/sep10/run_echo.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for xvar in $( cat input_values ) ; do
+    ..... collatz_one_number
+done
+
---
+++ sessions/sep10/talkback.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using std::cin;
+#include <print>
+using std::println;
+
+int main() {
+  int number;
+  cin >> number;
+  println("{}",number);
+  return 0;
+}
